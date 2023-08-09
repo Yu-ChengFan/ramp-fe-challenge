efa2eb8e02c00f460819f789d4d36a975da05377
Corny Mac <kevin130366@gmail.com>
2023-08-09 10:52:14 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,99 @@
+import { Fragment, useCallback, useEffect, useMemo, useState } from "react"
+import { InputSelect } from "./components/InputSelect"
+import { Instructions } from "./components/Instructions"
+import { Transactions } from "./components/Transactions"
+import { useEmployees } from "./hooks/useEmployees"
+import { usePaginatedTransactions } from "./hooks/usePaginatedTransactions"
+import { useTransactionsByEmployee } from "./hooks/useTransactionsByEmployee"
+import { EMPTY_EMPLOYEE } from "./utils/constants"
+import { Employee } from "./utils/types"
+
+export function App() {
+  const { data: employees, loading: employeesLoading, ...employeeUtils } = useEmployees()
+  const { data: paginatedTransactions, loading: transactionsLoading, ...paginatedTransactionsUtils } = usePaginatedTransactions()
+  const { data: transactionsByEmployee, ...transactionsByEmployeeUtils } = useTransactionsByEmployee()
+  const [isFilteredByEmployee, setIsFilteredByEmployee] = useState(false);
+
+  const transactions = useMemo(
+    () => paginatedTransactions?.data ?? transactionsByEmployee ?? null,
+    [paginatedTransactions, transactionsByEmployee]
+  )
+
+  const loadAllTransactions = useCallback(async () => {
+    setIsFilteredByEmployee(false)
+    transactionsByEmployeeUtils.invalidateData()
+
+    await employeeUtils.fetchAll()
+    await paginatedTransactionsUtils.fetchAll()
+
+  }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
+
+  const loadTransactionsByEmployee = useCallback(
+    async (employeeId: string) => {
+      if (employeeId === EMPTY_EMPLOYEE.id) {
+        setIsFilteredByEmployee(false);
+      } else {
+        setIsFilteredByEmployee(true);
+      }
+      paginatedTransactionsUtils.invalidateData();
+      await transactionsByEmployeeUtils.fetchById(employeeId);
+    },
+    [paginatedTransactionsUtils, transactionsByEmployeeUtils]
+  );
+
+  useEffect(() => {
+    if (employees === null && !employeesLoading) {
+      loadAllTransactions()
+    }
+  }, [employeesLoading, employees, loadAllTransactions])
+
+  return (
+    <Fragment>
+      <main className="MainContainer">
+        <Instructions />
+
+        <hr className="RampBreak--l" />
+
+        <InputSelect<Employee>
+          isLoading={employeesLoading}
+          defaultValue={EMPTY_EMPLOYEE}
+          items={employees === null ? [] : [EMPTY_EMPLOYEE, ...employees]}
+          label="Filter by employee"
+          loadingLabel="Loading employees"
+          parseItem={(item) => ({
+            value: item.id,
+            label: `${item.firstName} ${item.lastName}`,
+          })}
+          onChange={async (newValue) => {
+            if (!newValue) {
+              return;
+            }
+            if (newValue === EMPTY_EMPLOYEE) {
+              await loadAllTransactions();
+              return;
+            }
+            await loadTransactionsByEmployee(newValue.id);
+          }}
+        />
+
+        <div className="RampBreak--l" />
+
+        <div className="RampGrid">
+          <Transactions transactions={transactions} />
+
+          {!isFilteredByEmployee && transactions !== null && paginatedTransactions?.nextPage !== null && (
+            <button
+              className="RampButton"
+              disabled={transactionsLoading || paginatedTransactions?.nextPage === null}
+              onClick={async () => {
+                await loadAllTransactions()
+              }}
+            >
+              View More
+            </button>
+          )}
+        </div>
+      </main>
+    </Fragment>
+  )
+}
---
+++ src/components/InputCheckbox/index.tsx
@@ -0,0 +1,31 @@
+import classNames from "classnames"
+import { useRef } from "react"
+import { InputCheckboxComponent } from "./types"
+
+export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+
+  return (
+    <div className="RampInputCheckbox--container" data-testid={inputId}>
+      <label htmlFor={inputId}
+        className={classNames("RampInputCheckbox--label", {
+          "RampInputCheckbox--label-checked": checked,
+          "RampInputCheckbox--label-disabled": disabled,
+        })}
+      />
+      <input
+        id={inputId}
+        type="checkbox"
+        className="RampInputCheckbox--input"
+        checked={checked}
+        disabled={disabled}
+        onChange={() => {
+          console.log("Checkbox onChange triggered");
+          console.log("Checkbox clicked!");
+          onChange(!checked);
+        }}
+        onClick={() => console.log("Direct click on checkbox!")}
+      />
+    </div>
+  )
+}
